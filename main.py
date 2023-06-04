464ee22010a5834766c40f55fd892eb7ea964119
Korvettik <korvet1988@yandex.ru>
2023-06-04 19:36:09 +0300

    v.0.0.1b

--- main.py
+++ main.py
@@ -53,10 +53,10 @@ tr_H = input('Введите H грузовика: ')
 tr_M = input('Введите M грузовика: ')
 
 if tr_name == tr_l == tr_W == tr_H == tr_M == '':
-    print('Вы не ввели никаких параметров. Устанавливаются значения по умолчанию (в мм):\na225df, 2000, 2000, 2000, 50\n')
-    truck = Truck('a225df', 2000, 2000, 2000, 11)
+    print('Вы не ввели никаких параметров. Устанавливаются значения по умолчанию (в мм):\na225df, 3000, 2000, 2000, 11\n')
+    truck = Truck('a225df', 3000, 2000, 2000, 11)
 else:
-    truck = Truck(tr_name, tr_l, tr_W, tr_H, tr_M)
+    truck = Truck(tr_name, int(tr_l), int(tr_W), int(tr_H), int(tr_M))
 
 p = input('Введите имя json файла с данными о грузе: ')
 if p == '':
@@ -168,7 +168,7 @@ def s_of_group(group: list) -> int:
     s = sum(map(lambda x: x.L * x.W, group)) + truck.W * 50 * (len(group)+1) + truck.L * 50 * 2
     return s
 
-def horizont_combine(group: list) -> list:
+def horizont_combine(group: list) -> bool:
     '''Если в группе грузов находится комбинация горизонтальных поворотов,
      чтобы умещаться в кузове грузовика - True, если нет - False'''
     group = group[1]
@@ -179,17 +179,22 @@ def horizont_combine(group: list) -> list:
     turned_group = copy.deepcopy(group)
     for i in range(len(group)):
         turned_item = copy.deepcopy(group[i])
-        if group[i].L > group[i].W:
+        if group[i].L > group[i].W and truck.W >= group[i].L + 100:
             turned_item.W = group[i].L
             turned_item.L = group[i].W
             turned_group[i] = turned_item
+        if group[i].W > group[i].L and truck.W <= group[i].W + 100:
+            turned_item.L = group[i].W
+            turned_item.W = group[i].L
+            turned_group[i] = turned_item
+    # общая длина
+    sum_l = sum(map(lambda x: x.L, turned_group)) + 50 * (len(turned_group)+1)
+    # print(f'sum_l {sum_l}')
+    # максимальная ширина
+    max_w = max(map(lambda x: x.W, turned_group)) + 100
+    # print(f'max_w {max_w}')
 
-    sum_l = sum(map(lambda x: x.L, group)) + 50 * (len(group)+1)
-    print(f'sum_l {sum_l}')
-    sum_w = sum(map(lambda x: x.W, group)) + 100
-    print(f'sum_w {sum_w}')
-
-    if sum_l > truck.L or sum_w > truck.W:
+    if sum_l > truck.L or max_w > truck.W:
         res = False
 
     return res
@@ -203,14 +208,15 @@ def horizont_combine(group: list) -> list:
 # ----- алгоритм загрузки грузовика ----------------
 
 items_by_mass_lst = items_by_mass(items_lst)  # список списков [масса, [группа]]
-print(items_by_mass_lst)
+copy_items_by_mass_lst = copy.deepcopy(items_by_mass_lst)
+# print(items_by_mass_lst)
 
 flag = True
 while flag:
     group = items_with_max_mass(items_by_mass_lst)
     group_mas = group[0]
 
-    print(group_mas, group)
+    # print(group_mas, group)
 
     # УСЛОВИЕ ЗАДАЧИ - выбрать группу по МАКСИМАЛЬНОЙ возможной массе
     if truck.M >= group_mas:  # Все ОК
@@ -224,14 +230,13 @@ while flag:
             if tr_s >= s_of_group(group[1]):  # есть шанс все уместить
 
                 # ЗДЕСЬ ЛОГИКА ПОВОРОТОВ В ГОРИЗОНТАЛЬНОЙ ПЛОСКОСТИ
-                print(f'ты здесь {group_mas, group}')
+                # print(f'ты здесь {group_mas, group}')
 
                 res_horizont_combine = horizont_combine(group)
                 if res_horizont_combine:
                     print(f'Загружать эту комбинацию грузов:\nОбщая масса {group[0]}, Комплект {group[1]}')
                     for item in group[1]:
                         print(f'Наименование: {item.name}\nДлина: {item.W}\nШирина: {item.L}')
-
                     flag = False
 
                 else:  # комбинации не влезают
